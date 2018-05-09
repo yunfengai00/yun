188b575ff205820ca711e22b2d6a962e4f30eb9e
yunfengai00 <39121780+yunfengai00@users.noreply.github.com>
2018-05-09 20:22:51 +0800

    我爱你哦哦00000000000！ 提交神：枫王

--- app/src/main/java/com/example/wwwsn/yy_git/MainActivity.java
+++ app/src/main/java/com/example/wwwsn/yy_git/MainActivity.java
@@ -8,5 +8,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我想回家
+        //我是正的想回家，回家，回家
     }
 }
